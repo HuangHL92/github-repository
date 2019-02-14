468f01c7e572d4709815f3d83d47fdc5015a913a
梁涛 <18651018439@153.com>
2019-02-14 19:15:55 +0800

    单表MP实例代码上传（查询实现、表单验证未实现）

--- ruoyi-system/src/main/java/com/ruoyi/system/service/impl/DepartmentServiceImpl.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/impl/DepartmentServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ruoyi.system.service.impl;
 
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -23,8 +24,26 @@ public class DepartmentServiceImpl extends ServiceImpl<DepartmentMapper, Departm
     @Override
     public List<Department> selectList(Department department) {
         QueryWrapper<Department> query = new QueryWrapper<>();
-
-
+        // 部门名称模糊查询
+        query.lambda().like(StrUtil.isNotBlank(department.getName()), Department::getName, department.getName());
+        // 部门级别
+        query.lambda().eq(StrUtil.isNotBlank(department.getGrade()), Department::getGrade, department.getGrade());
+        // 创办日期范围查询
+        String startEstablishmentDate = department.getParams().isEmpty() ? null : department.getParams().get("startEstablishmentDate").toString();
+        if (StrUtil.isNotBlank(startEstablishmentDate)) {
+            query.lambda().ge(Department::getEstablishmentDate, DateUtil.parse(startEstablishmentDate, "yyyy-MM-dd"));
+        }
+        String endEstablishmentDate = department.getParams().isEmpty() ? null : department.getParams().get("endEstablishmentDate").toString();
+        if (StrUtil.isNotBlank(endEstablishmentDate)) {
+            query.lambda().le( Department::getEstablishmentDate, DateUtil.parse(endEstablishmentDate, "yyyy-MM-dd"));
+        }
+        // 关键字模糊查询（联系人/联系电话/联系地址）
+        String keyword = department.getParams().isEmpty() ? null : department.getParams().get("keyword").toString();
+        query.lambda().and(
+                StrUtil.isNotBlank(keyword),
+                i -> i.like(Department::getContact, keyword)
+                        .or().like(Department::getContactNumber, keyword)
+                        .or().like(Department::getContactAddress, keyword));
         return list(query);
     }
 }
