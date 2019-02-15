937f9a41eb6dac8ddfa3ade9b5e56a84136dfbdf
yufei <fei.yu@51e.com.cn>
2019-02-15 10:35:32 +0800

    echart集成

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/demo/MapController.java
@@ -0,0 +1,16 @@
+package com.ruoyi.web.controller.demo;
+
+import com.ruoyi.framework.web.base.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 地图实例demo
+ *
+ * @author ruoyi
+ */
+@Controller
+@RequestMapping("/demo/map")
+public class MapController extends BaseController {
+
+}
