bfa6d0a38340f19e146d881255e3b2fc9869b5fa
梁涛 <18651018439@153.com>
2019-02-21 18:06:28 +0800

    ehcache&redis文档说明

--- ruoyi-framework/src/main/java/com/ruoyi/framework/shiro/cache/RedisCacheManager.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/shiro/cache/RedisCacheManager.java
@@ -46,13 +46,13 @@ public class RedisCacheManager implements CacheManager
                 else if (CacheUtils.SHIRO_ACTIVE_SESSION_CACHE.equals(name)) {
                     cache = new RedisCache<>(name, 1800, redisManager);
                 }
-                // 授权缓存：60分钟
-                else if (CacheUtils.AUTHORIZATION_CACHE.equals(name)) {
-                    cache = new RedisCache<>(name, 3600, redisManager);
-                }
-                // 默认缓存：持久
-                else {
+                // 用户缓存：持久
+                else if (CacheUtils.USER_CACHE.equals(name)) {
                     cache = new RedisCache<>(name, -1, redisManager);
+                }
+                // 默认缓存（授权缓存）：60分钟
+                else {
+                    cache = new RedisCache<>(name, 3600, redisManager);
                 }
                 caches.put(name, cache);
             }
