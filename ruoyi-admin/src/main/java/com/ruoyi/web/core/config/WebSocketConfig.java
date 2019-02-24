a4aabe903d1bc956e1ec3bec3efbed06a5997f64
yufei <fei.yu@51e.com.cn>
2019-02-24 17:04:15 +0800

    tomcat下部署配置

--- ruoyi-admin/src/main/java/com/ruoyi/web/core/config/WebSocketConfig.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/core/config/WebSocketConfig.java
@@ -11,6 +11,7 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 @Configuration
 public class WebSocketConfig {
 
+//    TODO 如果用外置tomcat，要注释掉以下代码，否则启动项目会报错，用springboot内置tomcat就得放开以下代码
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
