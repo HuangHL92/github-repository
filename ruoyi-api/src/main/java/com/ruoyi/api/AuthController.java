6f41256acac76a44ee850552824850169dcc6c6f
yufei <fei.yu@51e.com.cn>
2019-04-13 15:08:00 +0800

    API接口完善

--- ruoyi-api/src/main/java/com/ruoyi/api/AuthController.java
+++ ruoyi-api/src/main/java/com/ruoyi/api/AuthController.java
@@ -3,20 +3,22 @@ package com.ruoyi.api;
 import com.ruoyi.base.ApiBaseController;
 import com.ruoyi.common.base.ApiResult;
 import com.ruoyi.common.enums.ResponseCode;
-//import com.ruoyi.common.utils.JedisUtils;
+import com.ruoyi.common.exception.user.CaptchaException;
+import com.ruoyi.common.utils.JedisUtils;
 import com.ruoyi.framework.jwt.domain.Account;
 import com.ruoyi.framework.jwt.service.TokenService;
+import com.ruoyi.system.domain.SysUser;
+import com.ruoyi.system.service.ISysUserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.crypto.hash.Md5Hash;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 
 /**
@@ -32,6 +34,9 @@ public class AuthController extends ApiBaseController {
 
     @Autowired
     private TokenService tokenService;
+
+    @Autowired
+    private ISysUserService userService;
 
     @Value("${api.token.expires}")
     private int  expires;
@@ -88,8 +93,7 @@ public class AuthController extends ApiBaseController {
         }
 
         //3.生成令牌写入redis
-//        String token  =JedisUtils.get(String.format(TOKEN_KEY,account));
-        String token = "";
+        String token  =JedisUtils.get(String.format(TOKEN_KEY,account));
         //多少分钟后过期
         int etime = expires*60*1000;
         //过期时间
@@ -119,7 +123,7 @@ public class AuthController extends ApiBaseController {
         ac.setUsername(account);
         ac.setPassword(password);
         String token = tokenService.getToken(ac);
-       // JedisUtils.set(String.format(TOKEN_KEY,account), token,exptime);
+        JedisUtils.set(String.format(TOKEN_KEY,account), token,exptime);
         return token;
     }
 
@@ -130,21 +134,32 @@ public class AuthController extends ApiBaseController {
      * @return
      */
     private boolean checkAccount(String account, String password) {
-//        SysUser user = null;
-//        try
-//        {
-//            user = loginService.login(account, password);
-//        }
-//        catch (CaptchaException e)
-//        {
-//            throw new AuthenticationException(e.getMessage(), e);
-//        }
-//        if(user==null) {
-//            return false;
-//        }
+        SysUser user = null;
+        try
+        {
+            user = userService.selectUserByLoginName(account);
+
+            if(user ==null || !user.getPassword().equals(encryptPassword(account,password,user.getSalt()))) {
+                return false;
+            }
+        }
+        catch (CaptchaException e)
+        {
+            throw e;
+        }
+        if(user==null) {
+            return false;
+        }
 
         return true;
     }
+
+
+    private String encryptPassword(String username, String password, String salt)
+    {
+        return new Md5Hash(username + password + salt).toHex().toString();
+    }
+
 
 
 }
